5b197c5f7c05811e8e4f07cff67f32443e418d6f
bukrain <querieth21@gmail.com>
2019-07-01 19:48:11 +0200

    Cleanup

--- sample/image_downloader.py
+++ sample/image_downloader.py
@@ -1,10 +1,6 @@
 import praw
 import prawcore as pwc
-import datetime as dt
-import urllib.request as req
-import urllib.error as reqError
 import os
-import time
 import logging
 import requests
 import json
@@ -41,18 +37,13 @@ class ImageDownloader:
             with open(self.DATA_FILE, "w") as f:
                 json.dump(data,f)
 
-    def get_time(self, created):
-        return dt.datetime.timestamp(created)
-
-    def get_content_type(self, info):
-        for item in info.split("\n"):
-            if item.startswith("Content-Type"):
-                ctype = item.split(' ')[1].split('/')
-                if ctype[0] == "image":
-                    return ctype[1]
-                elif ctype[0] == "video":
-                    return ctype[1]
-        return "No"
+    def is_downloadable(self, url):
+        head = requests.head(url, allow_redirects=True)
+        header = head.headers
+        content_type = header.get('content-type')
+        if 'text' in content_type.lower() or 'html' in content_type.lower():
+            return False
+        return True
 
     def sub_exists(self, subreddit_name, reddit):
         exists = True
@@ -79,6 +70,7 @@ class ImageDownloader:
         
         response = requests.request('GET', api_url, headers = headers, allow_redirects=False)
         if(response.status_code == 404):
+            print(api_url)
             raise NotFoundError("Image not found")
         resp_json = response.json()
         if 'X-RateLimit-ClientRemaining' in response.headers.keys():
@@ -119,6 +111,10 @@ class ImageDownloader:
             for submission in submissions:
                 if "imgur" in submission.url:
                     self.download_from_imgur(submission.url, subreddit.display_name, submission.id)
+                elif "gfycat" in submission.url:
+                    response = requests.request('GET',submission.url)
+                    print('\n\n\n\n')
+                    print(type(response.content))
                 else:
                     self.download_from_reddit(submission.url, subreddit.display_name, submission.id)
         else:
@@ -129,18 +125,26 @@ class ImageDownloader:
 
     def download_from_imgur(self, url_of_image, name_of_subreddit, id_of_submission):
         url_of_images = {}
-        try:
-            if self.api_data["imgur_data"]["remaining_user"] <= self.api_data["imgur_data"]["user_limit"]:
-                raise UserLimitError('Too many requests per hour')
-            if self.api_data["imgur_data"]["remaining_client"] <= self.api_data["imgur_data"]["client_limit"]:
-                raise ClientLimitError('Too many request per day')
-        except UserLimitError:
-            logging.warning('You have made too many requests to imgur per hour {0}'.format(url_of_image))
-        except ClientLimitError:
-            logging.warning('You have made too many requests to imgur per day {0}'.format(url_of_image))
-        
-        try:
+        if '/gallery/' in url_of_image or '/a/' in url_of_image:
+            try:
+                if self.api_data["imgur_data"]["remaining_user"] <= self.api_data["imgur_data"]["user_limit"]:
+                    raise UserLimitError('Too many requests per hour')
+                if self.api_data["imgur_data"]["remaining_client"] <= self.api_data["imgur_data"]["client_limit"]:
+                    raise ClientLimitError('Too many request per day')
+            except UserLimitError:
+                logging.warning('You have made too many requests to imgur per hour {0}'.format(url_of_image))
+            except ClientLimitError:
+                logging.warning('You have made too many requests to imgur per day {0}'.format(url_of_image))
             url_of_images = self.get_imgur_image_url(url_of_image)
+        else:
+            if 'i.imgur' in url_of_image:
+                id = url_of_image.split('/')[3]
+                url_of_images = { id: url_of_image}
+            else:
+                id = url_of_image.split('/')[3] 
+                url_of_images={ id: 'https://i.imgur.com/' + id}
+
+        try:
             for key in url_of_images:
                 id = "{0}_{1}".format(id_of_submission, key)
                 image_url = url_of_images[key]
@@ -148,23 +152,19 @@ class ImageDownloader:
                     image_url = image_url[0:len(image_url) - 4] + 'mp4'
                 self.download_image(image_url, name_of_subreddit, id)
         except NotFoundError:
-            logging.error('Image not found 404 {0}'.format(url_of_image))   
+            logging.error('Image not found 404 {0}'.format(url_of_image))
 
     def download_image(self, url_of_image, name_of_subreddit, id_of_submission):
-        try:
-            dir = self.IMG_DIR + name_of_subreddit + "\\" 
-            with req.urlopen(url_of_image) as url:
-                try:
-                    info = url.info().as_string()
-                except reqError.URLError:
-                    logging.warning('Can\'t get header of {0}'.format(url_of_image))
-                content_type = self.get_content_type(info)
-                if content_type != "No":
-                    id = id_of_submission
-                    with open(dir + id + '.' + content_type, "b+w") as f:
-                        f.write(url.read())
-                        logging.info('Image: {0} downloaded from {1}'.format(id, url_of_image))
-                else:
-                    logging.info('Not image or video {0}'.format(url_of_image))
-        except reqError.URLError:
-            logging.warning('Can\'t open {0}'.format(url_of_image))
+        dir = self.IMG_DIR + name_of_subreddit + "\\"
+        if self.is_downloadable(url_of_image):
+            id = id_of_submission
+            response = requests.get(url_of_image)
+            content_type = response.headers.get('content-type').split('/')[1]
+            with open(dir + id + '.' + content_type, "b+w") as f:
+                f.write(response.content)
+                logging.info('Image: {0} downloaded from {1}'.format(id, url_of_image))
+        else:
+            logging.info('Not image or video {0}'.format(url_of_image))
+
+imageDownloader = ImageDownloader()
+imageDownloader.get_images('gifs')
